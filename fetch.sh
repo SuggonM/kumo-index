f22800f0e727fac08415e7f15ddc92709145ba74
SuggonM <maharjansagun99@gmail.com>
2025-06-17 19:52:40 +0545

    workflow for checking version update daily
    i've set runner to be called from the main branch
    ref: https://github.com/orgs/community/discussions/16107#discussioncomment-8786272

---
+++ fetch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl="https://kumo.pro.g123-cpp.com/prod/kumo/version.txt"
+upstream=$(curl -sL $curl | jq -r '.url[0]')
+current=$(tail -1 README.md | tail -c +4)
+
+if [[ "$upstream" != "$current" ]]; then
+	echo "1. $upstream" >> README.md
+fi
+
+export upstream
